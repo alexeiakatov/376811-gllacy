17844336a6d3ca9f537e6f575ebf5515e106e0d4
Alexei Akatov <alexeiakatov@gmail.com>
2017-09-12 14:55:08 +0500

    Gllacy : сделано что у всех полей ввода исчезает плейсхолдер когда они получают фокус.

--- js/script.js
+++ js/script.js
@@ -8,8 +8,16 @@ var dark = document.querySelector(".darkLayer");
 //кнопка закрытия попапа обратной связи "крестик"
 var closeFormButton = document.querySelector(".closeFormButton");
 
-//поле ввода имени клиента
+//поля ввода имени, эл.почты и сообщения
 var clientName = dark.querySelector(".clientName");
+var clientEmail = dark.querySelector(".clientEmail");
+var clientMessage = dark.querySelector(".getMessage");
+
+//поля ввода поиска, логина, пароля и эл.почты для подписки на рассылку
+var searchField = document.querySelector(".searchField");
+var login = document.querySelector(".login");
+var password = document.querySelector(".password");
+var inputEmail = document.querySelector(".inputEmail");
 
 //ссылка с изображением обычной карты div.mapImage
 var mapImage = document.querySelector(".mapImage");
@@ -58,4 +66,60 @@ mapImage.addEventListener("click", function(clickEvent){
 
 });
 
+    /*ОБРАБОТКА СОБЫТИЙ ПОЛУЧЕНИЯ ФОКУСА ЭЛЕМЕНТАМИ ВВОДА*/
 
+//поле clientName
+clientName.addEventListener("focus", function(focusEvent){
+  clientName.removeAttribute("placeholder");
+});
+clientName.addEventListener("blur", function(blurEvent) {
+  clientName.setAttribute("placeholder", "Как вас зовут?");
+});
+
+//поле clientEmail
+clientEmail.addEventListener("focus", function(focusEvent){
+  clientEmail.removeAttribute("placeholder");
+});
+clientEmail.addEventListener("blur", function(blurEvent) {
+  clientEmail.setAttribute("placeholder", "Ваша почта для ответа?");
+});
+
+//поле getMessage
+clientMessage.addEventListener("focus", function(focusEvent){
+  clientMessage.removeAttribute("placeholder");
+});
+clientMessage.addEventListener("blur", function(blurEvent) {
+  clientMessage.setAttribute("placeholder", "Напишите что-нибудь...");
+});
+
+//поле searchField
+searchField.addEventListener("focus", function(focusEvent){
+  searchField.removeAttribute("placeholder");
+});
+searchField.addEventListener("blur", function(blurEvent) {
+  searchField.setAttribute("placeholder", "Что ищем?");
+});
+
+//поле login
+login.addEventListener("focus", function(focusEvent){
+  login.removeAttribute("placeholder");
+});
+login.addEventListener("blur", function(blurEvent) {
+  login.setAttribute("placeholder", "Электронная почта");
+});
+
+//поле password
+password.addEventListener("focus", function(focusEvent){
+  password.removeAttribute("placeholder");
+});
+password.addEventListener("blur", function(blurEvent) {
+  password.setAttribute("placeholder", "Пароль");
+});
+
+//поле inputEmail
+inputEmail.addEventListener("focus", function(focusEvent){
+  inputEmail.removeAttribute("placeholder");
+});
+inputEmail.addEventListener("blur", function(blurEvent) {
+  inputEmail.setAttribute("placeholder", "Электронная почта");
+});
